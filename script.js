1534103dcbcff9a959f422dd5bf56d06b93deada
mishka <mishka.sakhelashvili@gmail.com>
2022-02-23 17:59:32 +0400

    addd

--- script.js
+++ script.js
@@ -19,19 +19,22 @@ slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
 // const link = document.querySelector('.left-slide');
 // link.addEventListener('mouseup', () => changeSlide('up'));
+// upButton.classList.add("hide");
+
+
 
 
 
 upButton.addEventListener('click', () => changeSlide('down'));
 downButton.addEventListener('click', () => changeSlide('up'));
-document.addEventListener('wheel', function (event) {
-    if (event.deltaY < 0) {
-        changeSlide('down');
-    }
-    else if (event.deltaY > 0) {
-        changeSlide('up');
-    }
-});
+// document.addEventListener('wheel', function (event) {
+//     if (event.deltaY < 0) {
+//         changeSlide('down');
+//     }
+//     else if (event.deltaY > 0) {
+//         changeSlide('up');
+//     }
+// });
 
 document.addEventListener('keydown', (e) => e.keyCode === 40 && changeSlide('up'));
 document.addEventListener('keydown', (e) => e.keyCode === 38 && changeSlide('down'));
@@ -41,22 +44,43 @@ document.addEventListener('keydown', (e) => e.keyCode === 27 && changeSlide('top
 // test.addEventListener('click', () => changeSlide('skills'));
 // გადამისამართება მენიუთი
 
-
+function hide(element) {
+    element.classList.add("hide");
+}
+function show(element) {
+    element.classList.remove("hide");
+}
+hide(upButton);
 
 const changeSlide = (direction) => {
     const sliderHeight = sliderContainer.clientHeight;
     if (direction === 'up') {
         activeSlideIndex++;
-        if (activeSlideIndex > slidesLength - 1) {
-            activeSlideIndex = 0;
+        show(upButton);
+        if (activeSlideIndex === 0) {
+            hide(upButton);
         }
-    } else if (direction === 'down') {
-        activeSlideIndex--;
-        if (activeSlideIndex < 0) {
+        if (activeSlideIndex === slidesLength - 1) {
+            hide(downButton);
+        }
+        if (activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = slidesLength - 1;
         }
+    }
+
+    else if (direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex === 0) {
+            hide(upButton);
+        }
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = 0;
+        }
+        if (activeSlideIndex < slidesLength - 1) {
+            show(downButton);
+        }
     } else if (direction === 'top') {
-        activeSlideIndex = 0;
+        // activeSlideIndex = 0;
     }
     // else if (direction === 'skills') {
     //     activeSlideIndex = 6;
@@ -64,8 +88,6 @@ const changeSlide = (direction) => {
     // }
     slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
     slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
-
-    console.log(activeSlideIndex);
 };
 
 
